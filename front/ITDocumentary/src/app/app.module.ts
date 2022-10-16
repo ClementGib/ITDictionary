1280d8d9f98d5c0058c271fd2e8b081263d9b0ac
CodingClem <c.gibertpro@gmail.com>
2022-10-17 00:33:54 +0200

    Dictionary first conception with JSON Object

--- front/ITDocumentary/src/app/app.module.ts
+++ front/ITDocumentary/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { DefinitionsComponent } from './definitions/definitions.component';
+import { DefinitionComponent } from './definition/definition.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    DefinitionsComponent,
+    DefinitionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ front/ITDocumentary/src/app/definitions/definitions.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Definition } from '../model/definition';
+import definitions from '../../assets/json/definitions.json'
+
+@Component({
+  selector: 'app-definitions',
+  templateUrl: './definitions.component.html',
+  styleUrls: ['./definitions.component.scss']
+})
+export class DefinitionsComponent implements OnInit {
+
+  public definitions: Definition[] = definitions;
+
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit(): void {
+  }
+
+}
